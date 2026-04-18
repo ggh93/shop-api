835ce845f81d51d83d6182da8c089041e95125a0
ggh93 <ggh93@naver.com>
2026-04-18 23:54:57 +0900

    feat: 스웨거문서 추가

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = NestFactory.create(AppModule);
-  (await app).useGlobalPipes(
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  (await app).listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Shop API')
+    .setDescription('커머스 주문/결제 API 서버')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('🔥 Server bootstrap failed', err);
+  process.exit(1);
+});
